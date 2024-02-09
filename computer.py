59569b09b62a9604df153d607d4d36a181b7bce3
Olohi John <ojohn@smith.edu>
2024-02-08 21:32:11 -0500

    Finalized computer.py

--- computer.py
+++ computer.py
@@ -1,6 +1,8 @@
+
+#Defines the computer class
 class Computer:
 
-    # What attributes will it need?
+    # Attributes of class Computer
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -9,8 +11,7 @@ class Computer:
     year_made: int
     price: int
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+    # Initializes constructor for the Computer class
     def __init__(self,
                 description: str,
                 processor_type: str,
@@ -19,7 +20,7 @@ class Computer:
                 operating_system: str,
                 year_made: int,
                 price: int):
-        
+        #Assigns the attribute x to variable self.x
         self.description = description
         self.processor_type = processor_type
         self.hard_drive_capacity = hard_drive_capacity
@@ -28,33 +29,22 @@ class Computer:
         self.year_made = year_made
         self.price = price
 
-    def create_computer(self):
-        global itemID
-        itemID += 1
-        return {itemID: {self.description, self.processor_type, self.hard_drive_capacity, 
-                self.memory, self.operating_system, self.year_made, self.price}}    
-
-
-
-    #What methods will you need?
+    #Prints the created Computer instances
     def __str__(self):
-        return (f"{self.description}, {self.processor_type}, {self.processor_type}, {self.hard_drive_capacity}, {self.memory}, {self.operating_system}, {self.year_made}, {self.price}") 
+        return (f"{self.description}, {self.processor_type}, {self.hard_drive_capacity}, {self.memory}, {self.operating_system}, {self.year_made}, {self.price}") 
     
-
-        
-    # def update(self, itemID: int, new_os: str):
-    #     if itemID in ResaleShop.inventory:
-    #         computer = ResaleShop.inventory[itemID] # locate the computer
-    #         if new_os is not None:
-    #             computer["operating_system"] = new_os # update details after installing new OS
-    #     else:
-    #         print("Item", itemID, "not found. Please select another item to refurbish.")
+    #Returns the year associated with a given computer instance--- not necessary for now
+    def year_made(self):
+        return (self.year_made)
             
-        
+#main() program to test the Computer class       
 def main():
-    computer1 = Computer("MacAir", "gig", 23, 16, "OS", 2024, 500)
+    #Creates a Computer instance, computer1
+    computer1 = Computer("MacAir", "gig", 23, 16, "OS", 2024, 500) 
     print(computer1)
 
+
+#Prevents function from running when imported to another function
 if "__main__" == __name__:
     main()
    
