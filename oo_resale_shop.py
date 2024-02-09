238ed40d0e34e998995474d3d1cd8aa66e0a79b1
Olohi John <ojohn@smith.edu>
2024-02-08 21:32:51 -0500

    Finalized resaleshopoop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,20 +1,20 @@
+#Imports from the typing module methods Dict and Optional
 from typing import Dict, Optional
+#Imports the class Computer from file with filename computer.py in the folder
 from computer import Computer
 
+itemID = 0 #Sets itemID to 0, increments it by 1 after every computer instance is added
 
-itemID = 0
+
+#Defines the ResaleShop class
 class ResaleShop:
 
-    # What attributes will it need?
-    inventory : {}
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+    #Initializes the required attribute for the ResaleShop class
+    inventory : Dict[int, Dict] = {}
     def __init__(self, inventory):
         self.inventory = inventory
 
-
-    # What methods will you need?
-        
+    #Defines the buy function to add an item to the inventory    
     def buy(self, computer: Dict):
         global itemID
         itemID += 1 # increment itemID
@@ -23,6 +23,7 @@ class ResaleShop:
         #print(self.inventory)
         return itemID
     
+    #Defines the sell function to remove an item from the inventory   
     def sell(self, computerID: int):
         if computerID in self.inventory:
             del self.inventory[computerID]
@@ -30,49 +31,79 @@ class ResaleShop:
         else: 
             print("Item", computerID, "not found. Please select another item to sell.")
 
-    def update_price(self, computerID: int, new_os: Optional[str] = None):
-        if computerID in self.inventory:
-            computer = self.inventory[computerID] # locate the computer
-            if int(computer["year made"]) < 2000:
-                computer["price"] = 0 # too old to sell, donation only
-            elif int(computer["year_made"]) < 2012:
-                computer["price"] = 250 # heavily-discounted price on machines 10+ years old
-            elif int(computer["year_made"]) < 2018:
-                computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
-            else:
-                computer["price"] = 1000 # recent stuff
-            
-    # def refurbish_OS
 
-    #         if new_os is not None:
-    #             computer["operating_system"] = new_os # update details after installing new OS
-    #     else:
-    #         print("Item", item_id, "not found. Please select another item to refurbish.")
-
-
+    #Prints items in the inventory
     def print_inventory(self):
-    # If the inventory is not empty
+        # If the inventory is not empty
         if self.inventory:
             # For each item
             for computerID in self.inventory:
                 # Print its details
-                print(f'Item ID: {computerID} : {self.inventory[computerID]}')
+                print(f'Item ID: {computerID}, Properties: ({self.inventory[computerID]})')
         else:
-            print("No inventory to display.")
+            print("Inventory currently empty. No inventory to display.")
 
+#Program parts I really need help with
+#Whenever I try running the update_price, it says computer object "price" cannot be assigned
+    """def update_price(self, computerID: int, new_price: int):
+        if computerID in self.inventory:
+            self.inventory[computerID]["price"] = new_price
+        else:
+            print("Item", computerID, "not found. Cannot update price.")"""
     
-        
+#Similarly, running this function says Computer object not subscriptable
+    """def refurbish(self, computerID: int, new_os: Optional[str] = None):
+    if computerID in self.inventory:
+        computer = self.inventory[computerID] # locate the computer
+        if int(computer["year_made"]) < 2000:
+            computer["price"] = 0 # too old to sell, donation only
+        elif int(computer["year_made"]) < 2012:
+            computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+        elif int(computer["year_made"]) < 2018:
+            computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            computer["price"] = 1000 # recent stuff
 
+        if new_os is not None:
+            computer["operating_system"] = new_os # update details after installing new OS
+    else:
+        print("Item", computerID, "not found. Please select another item to refurbish.")"""
+    
+
+#main functin that implements the methods defined in the ResaleShop class
 def main():
-    computer1 = Computer("MacAir", "gig", 23, 16, "OS", 2024, 500)
+    #Creates two computers, computer1 and computer2
+    computer1 = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64,"macOS Big Sur", 2013, 1500)
+    computer2 = Computer("MacAir", "3.5 GHc 12-Core Intel Neon F6", 512, 256,"macOS Big Sur", 2023, 1500)
+    
+    #Creates an instance of the ResaleShop class
     resale = ResaleShop({})
-    computerID = resale.buy(computer1)
+    print("-----Loading inventory------")
+    resale.print_inventory() #prints items in inventory
 
-    resale.update_price(computerID)
-    #resale.sell(computerID)
+    print("-----Buying 1st computer-----")
+    computerID = resale.buy(computer1) #adds computer1 to inventory
+
+    print("-----1st Computer bought-----")
+    print()
+    resale.print_inventory() #prints items in inventory
+
+    print("----Buying second computer----")
+    computerID = resale.buy(computer2) #adds computer2 to inventory
+
+    print("-----2nd Computer bought-----")
+    print()
+    resale.print_inventory() #prints items in inventory
+    
+    print("-----Selling computer 1-----")
+    resale.sell(1) #Sells computer1
+
+    print() #prints a new line
+    print("----items left in inventory:-----")
+    resale.print_inventory() #prints items in inventory
     
 
-
+#Runs main function but prevents main() from running when package is imported to another program
 if "__main__" == __name__:
     main()
    
